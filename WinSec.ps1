a0b5e02bdaca9e222ce9dfedcbf6e360a9be2221
Schnee8558 <maxi.schneeberger@hotmail.com>
2025-03-17 07:43:03 +0100

    Update WinSec.ps1
    try2

--- WinSec.ps1
+++ WinSec.ps1
@@ -1,26 +1,12 @@
-Write-Host  -ForegroundColor Yellow "Custom OSDCloud ..."
-cls
-Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
-Write-Host "================ =================" -ForegroundColor Yellow
-Write-Host "==================== =====================" -ForegroundColor Yellow
-Write-Host "=======================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 22h2 | Deutsch | Pro"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
-Write-Host "3: I'll select it myself"-ForegroundColor Yellow
-Write-Host "4: Exit`n"-ForegroundColor Yellow
-$input = Read-Host "Please make a selection"
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+Start-Sleep -Seconds 5
 
-Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+Start-OSDCloud -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
 
-Import-Module OSD -Force
-Install-Module OSD -Force
+#Restart from WinPE
 
-switch ($input)
-{
-    '1' { Start-OSDCloud -OSLanguage de-de -OSBuild 22H2 -OSEdition Pro -ZTI -SkipPublisherCheck } 
-    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
-    '3' { Start-OSDCloud	} 
-    '4' { Exit		}
-}
+Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+
+Start-Sleep -Seconds 20
 
 wpeutil reboot
